c8f58496db108d545224d422881beeea971e0635
MajoEsparza <22151136@aguascalientes.tecnm.mx>
2025-02-10 07:51:23 -0600

    Added simple Layout components

--- app/src/main/java/com/example/clasetrabajo/MainActivity.kt
+++ app/src/main/java/com/example/clasetrabajo/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -19,8 +22,20 @@ class MainActivity : ComponentActivity() {
         /*enableEdgeToEdge()*/
         setContent {
             ClaseTrabajoTheme {
+                Row(){
+                    Column(){
                 Text(text = "Hello!")
                 Text(text = "Welcome to your first aplication!")
+                }
+                    Column(){
+                Text(text = "Hello!")
+                Text(text = "Welcome to your first aplication!")
+                }
+                    Column() {
+                        Text(text = "Hello!")
+                        Text(text = "Welcome to your first aplication!")
+                    }
+                }
                /*
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
