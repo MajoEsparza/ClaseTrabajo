a84506bf066756c47a2019ce9197594b7134f443
MajoEsparza <22151136@aguascalientes.tecnm.mx>
2025-03-17 17:55:56 -0600

    Changes to MaterialTheme color scheme

--- app/src/main/java/com/example/clasetrabajo/ui/theme/Color.kt
+++ app/src/main/java/com/example/clasetrabajo/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.example.clasetrabajo.ui.theme
 
+import android.hardware.lights.Light
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -10,3 +11,15 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 val Grey = Color(0xFFF1F1F1)
 //0xFFC5C0C0
+
+// Light Blue
+val LightBlue50 = Color(0xFFE1F5FE)
+val LightBlue100 = Color(0xFFB3E5FC)
+val LightBlue200 = Color(0xFF81D4FA)
+val LightBlue300 = Color(0xFF4FC3F7)
+val LightBlue400 = Color(0xFF29B6F6)
+val LightBlue500 = Color(0xFF03A9F4)
+val LightBlue600 = Color(0xFF039BE5)
+val LightBlue700 = Color(0xFF0288D1)
+val LightBlue800 = Color(0xFF0277BD)
+val LightBlue900 = Color(0xFF01579B)
--- app/src/main/java/screens/Theme.kt
+++ app/src/main/java/screens/Theme.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.example.clasetrabajo.ui.theme.LightBlue100
+import com.example.clasetrabajo.ui.theme.LightBlue900
 import com.example.clasetrabajo.ui.theme.Pink40
 import com.example.clasetrabajo.ui.theme.Pink80
 import com.example.clasetrabajo.ui.theme.Purple40
@@ -18,13 +20,14 @@ import com.example.clasetrabajo.ui.theme.PurpleGrey80
 import com.example.clasetrabajo.ui.theme.Typography
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = LightBlue900,
     secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+    tertiary = Pink80,
+
+    )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = LightBlue100,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
@@ -43,15 +46,9 @@ private val LightColorScheme = lightColorScheme(
 fun ClaseTrabajoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
